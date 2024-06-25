8c8404f97a92982e7d68993e6afe8e6c1d408e85
techyama <yamamoto.ikki.tech@gmail.com>
2024-06-25 14:53:20 +0000

    鍵登録

--- front/react_app/src/components/Input.tsx
+++ front/react_app/src/components/Input.tsx
@@ -1,6 +1,16 @@
 import { useState } from "react";
 
 export const Input = () => {
-  const [state, setState] = useState();
-  return <input type="text" value={state} />;
+  const [state, setState] = useState<any | null>();
+  return (
+    <>
+      <input
+        type="text"
+        onChange={(e) => {
+          setState(e.target.value);
+        }}
+      />
+      <input type="text" value={state} />
+    </>
+  );
 };
